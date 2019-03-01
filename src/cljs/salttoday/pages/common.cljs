3cc9dff26693ebc87f23172464d65455d6949e99
wsigouin <willsigouin@gmail.com>
2019-02-28 21:33:30 -0500

    Update common.cljs

--- src/cljs/salttoday/pages/common.cljs
+++ src/cljs/salttoday/pages/common.cljs
@@ -16,8 +16,9 @@
 (defn make-layout
   [current-page content]
   [:div.outer
-  [:div.content-wrapper [:div.navigation [nav-link "#/" "Home " current-page :home "icon-home3"] [nav-link "#/users" "Users " current-page :users "icon-group"]]
-   content [:img.title-image {:src "/img/soo-salty.png"}]]
-   [:img.footer-pile {:src "/img/footer-pile.png"}]
-   ]
-  )
+    [:div.content-wrapper 
+      [:div.navigation 
+				[nav-link "#/" "Home " current-page :home "icon-home3"] 
+        [nav-link "#/users" "Users " current-page :users "icon-group"]]
+        content [:img.title-image {:src "/img/soo-salty.png"}]]
+        [:img.footer-pile {:src "/img/footer-pile.png"}]])
