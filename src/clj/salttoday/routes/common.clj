719d6c7dbfb25a57b757a97173082d3cabb231e6
Tyler Wilding <xtvaser@gmail.com>
2019-03-09 17:13:42 -0500

    run cljfmt

--- src/clj/salttoday/routes/common.clj
+++ src/clj/salttoday/routes/common.clj
@@ -1,15 +1,15 @@
 (ns salttoday.routes.common
-    (:require [salttoday.layout :as layout]
-      [compojure.core :refer [defroutes GET PUT]]
-      [ring.util.http-response :as response]
-      [clojure.java.io :as io]
-      [salttoday.db.core :as db]))
+  (:require [salttoday.layout :as layout]
+            [compojure.core :refer [defroutes GET PUT]]
+            [ring.util.http-response :as response]
+            [clojure.java.io :as io]
+            [salttoday.db.core :as db]))
 
 (defroutes common-routes
-           (GET "/todays-stats" []
-                (-> (response/ok (db/get-todays-stats))
-                    (response/header "Content-Type"
-                                     "application/json"))))
+  (GET "/todays-stats" []
+    (-> (response/ok (db/get-todays-stats))
+        (response/header "Content-Type"
+                         "application/json"))))
 
 
 
