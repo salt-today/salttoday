df9e28fdccfba7febfed01629b78fb7ffd4de1bc
Robert Brown <robrownn@gmail.com>
2019-03-26 20:13:02 -0400

    added get-users function

--- src/cljs/salttoday/pages/user_leaderboard.cljs
+++ src/cljs/salttoday/pages/user_leaderboard.cljs
@@ -12,9 +12,11 @@
   (js/console.log response)
   (reset! state response))
 
-(GET "/top-users"
-  {:headers {"Accept" "application/transit"}
-   :handler top-users-handler})
+(defn get-users []
+  (if (empty? @state)
+    (GET "/top-users"
+      {:headers {"Accept" "application/transit"}
+       :handler top-users-handler})))
 
 (defn display-user
   [user]
@@ -41,6 +43,7 @@
 
 (defn users-page
   []
+  (get-users)
   [:div.page-wrapper
    (make-navbar :home)
    (make-content :home (leaderboard-content @state))
