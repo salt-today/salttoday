c1d5ccac78ab0323e0f924f7ccc1417b48d8fa14
Dan <danharasymiw@gmail.com>
2019-04-27 15:19:54 -0400

    Add comments endpoint

--- src/cljs/salttoday/pages/home.cljs
+++ src/cljs/salttoday/pages/home.cljs
@@ -5,36 +5,55 @@
             [salttoday.pages.common :refer [content make-content make-navbar make-right-offset jumbotron]]))
 
 (def state
-  (r/atom {}))
+  (r/atom {:daily []
+           :weekly []
+           :all-time []}))
 
-(defn top-comments-handler [response]
+(defn comments-handler [days-range response]
   (js/console.log response)
-  (reset! state response))
+  (swap! state assoc days-range response))
+
 
 (defn get-comments []
-  (if (empty? @state)
-    (GET "/top-comments"
-      {:headers {"Accept" "application/transit"}
-       :handler top-comments-handler})))
+  (if (empty? (:all-time @state))
+    (do (GET "/comments" {:params {:offset    0
+                                   :amount    3
+                                   :sort-type "score"
+                                   :days      1}
+                          :headers {"Accept" "application/transit"}
+                          :handler (partial comments-handler :daily)})
+        (GET "/comments" {:params {:offset 0
+                                   :amount 5
+                                   :sort-type "score"
+                                   :days 7}
+                          :headers {"Accept" "application/transit"}
+                          :handler (partial comments-handler :weekly)})
+        (GET "/comments" {:params {:offset 0
+                                   :amount 50
+                                   :sort-type "score"}
+                          :headers {"Accept" "application/transit"}
+                          :handler (partial comments-handler :all-time)}))))
+
+
 
 (defn home-content [snapshot]
   (list
         ; Temporary fix until Today functions properly
-   (if (not (empty? (get snapshot "daily")))
+   (if (not (empty? (:daily snapshot)))
      (list [:div.row.justify-center.header-wrapper
             [:span.heading "Today"]]
            [:div.column.justify-center.comments-wrapper
-            (for [comment (get snapshot "daily")]
+            (for [comment (:daily snapshot)]
               (display-comment comment))]))
    (list [:div.row.justify-center.header-wrapper
           [:span.heading "Weekly"]]
          [:div.column.justify-center.comments-wrapper
-          (for [comment (get snapshot "weekly")]
+          (for [comment (:weekly snapshot)]
             (display-comment comment))])
    [:div.row.justify-center.header-wrapper
     [:span.heading "All Time"]]
    [:div.column.justify-center.comments-wrapper
-    (for [comment (get snapshot "all-time")]
+    (for [comment (:all-time snapshot)]
       (display-comment comment))]))
 
 (defn home-page []
