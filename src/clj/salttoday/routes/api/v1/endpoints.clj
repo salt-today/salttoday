83abb4559bb6f4d70cb9e40f35eb65253237c036
Tyler Wilding <xtvaser@gmail.com>
2019-06-28 19:50:54 -0400

    stash

--- src/clj/salttoday/routes/api/v1/endpoints.clj
+++ src/clj/salttoday/routes/api/v1/endpoints.clj
@@ -8,6 +8,25 @@
             [salttoday.db.core :as db]
             [clojure.tools.logging :as log]))
 
+; TODO Once we avoid the whole - passing in 0 instead of nil / the lack of a key
+; we should make the parsing functions not require strings
+(defn maybe [{:keys [offset amount sort-type days search-text user id deleted]
+              :or {offset "0"
+                   amount "50"
+                   sort-type "score"
+                   days "1"
+                   id nil
+                   deleted "false"}}]
+  (let [offset-num (routing-util/string->number offset)
+        amount-num (routing-util/string->number amount)
+        days-num (routing-util/string->number days)
+        id-num (routing-util/string->number id)
+        deleted-bool (routing-util/string->bool deleted)
+        results (db/get-top-x-comments offset-num amount-num sort-type days-num search-text user id-num deleted-bool)]
+    (-> (response/ok results)
+        (response/header "Content-Type"
+                         "application/json"))))
+
 (defroutes endpoints
   (GET "/api/v1/todays-stats" []
     (honeycomb/send-metrics {"api-hit" "todays-stats"})
@@ -15,17 +34,8 @@
         (response/header "Content-Type"
                          "application/json")))
 
-  ; TODO Would be better if we passed in nil instead of 0, see string->number function.
-  (GET "/api/v1/comments" [offset amount sort-type days search-text user id deleted]
-    (let [offset-num (routing-util/string->number offset)
-          amount-num (routing-util/string->number amount)
-          days-num (routing-util/string->number days)
-          id-num (routing-util/string->number id)
-          deleted-bool (routing-util/string->bool deleted)
-          results (db/get-top-x-comments offset-num amount-num sort-type days-num search-text user id-num deleted-bool)]
-      (-> (response/ok results)
-          (response/header "Content-Type"
-                           "application/json"))))
+  (GET "/api/v1/comments" [params]
+    (maybe params))
 
   (GET "/api/v1/users" [offset amount sort-type days]
     (honeycomb/send-metrics {"api-hit" "top-users"})
