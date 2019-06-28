cde1858c41e153c773d373e987cae73192a2a2e8
Tyler Wilding <xtvaser@gmail.com>
2019-06-28 01:13:02 -0400

    No longer load article page to determine title
    Closes #69 and #7

--- src/clj/salttoday/scraper.clj
+++ src/clj/salttoday/scraper.clj
@@ -11,46 +11,63 @@
         (for [sub substrings]
           (.startsWith string sub))))
 
-(defn ^:private get-url-from-div
-  [div]
+(defn ^:private get-url-from-article-link
+  [link]
   (str "https://www.sootoday.com"
-       (-> div
-           (html/select [:a])
-           first
+       (-> link
            :attrs
            :href)))
 
-(defn ^:private get-id-from-div
-  [div]
-  (-> div
-      (html/select [:a])
-      first
+(defn ^:private get-id-from-article-link
+  [link]
+  (-> link
       :attrs
       :data-id))
 
-(defn ^:private get-title-from-div
-  [div]
-  (let [contents (-> div first :content)
-        content-text-list (for [content contents]
-                            (-> (if (map? content)
-                                  (-> content :content first)
-                                  content)
-                                .trim))]
-    (clojure.string/join " " content-text-list)))
+(defn safe-trim [str]
+  (if (nil? str)
+    nil
+    (str/trim str)))
 
-(defn ^:private get-articles-from-homepage
+(defn render-node
+  [node]
+  (let [contents (-> node :content)
+        content-text-list (for [content contents]
+                            (safe-trim (if (map? content)
+                                         ; TODO - im not terribly thrilled about this
+                                         (when (not= (:tag content) :div)
+                                           (-> content :content first))
+                                         content)))]
+    (safe-trim
+     (clojure.string/join " " content-text-list))))
+
+(defn select-links-and-titles [html-payload]
+  (map vector (html/select html-payload [:a.section-item])
+       (html/select html-payload [:div.section-title])))
+
+(defn get-articles-from-homepage
   []
-  (let [article-divs (-> (html/html-snippet
-                          (:body @(http/get "https://www.sootoday.com" {:insecure? true})))
-                         (html/select [[:a (html/attr? :data-id)]]))
-        titles-and-urls (for [div article-divs]
-                          {:url (get-url-from-div div)
-                           :id (get-id-from-div div)})]
-    (filter #(starts-with-strings (:url %)
-                                  ["https://www.sootoday.com/local-news/" "https://www.sootoday.com/spotlight/"
-                                   "https://www.sootoday.com/great-stories/" "https://www.sootoday.com/videos/"
-                                   "https://www.sootoday.com/local-sports/" "https://www.sootoday.com/local-entertainment/"])
-            titles-and-urls)))
+  (let [html-payload (html/html-snippet
+                      (:body @(http/get "https://www.sootoday.com" {:insecure? true})))
+        link-and-title-elements (select-links-and-titles html-payload)
+        titles-and-urls (for [pair link-and-title-elements
+                              :let [link (first pair)
+                                    title (second pair)]]
+                          {:url (get-url-from-article-link link)
+                           :id (get-id-from-article-link link)
+                           :title (render-node title)})]
+    titles-and-urls
+    (set
+     (filter #(starts-with-strings (:url %)
+                                   ["https://www.sootoday.com/local-news/"
+                                    "https://www.sootoday.com/spotlight/"
+                                    "https://www.sootoday.com/great-stories/"
+                                    "https://www.sootoday.com/videos/"
+                                    "https://www.sootoday.com/local-sports/"
+                                    "https://www.sootoday.com/local-entertainment/"
+                                    "https://www.sootoday.com/bulletin/"
+                                    "https://www.sootoday.com/more-local/"])
+             titles-and-urls))))
 
 (defn ^:private get-content-helper
   "Gets the content of username, text, upvotes, downvotes, etc"
@@ -187,7 +204,6 @@
     (html/select html [:div#comments :> :div.comment])))
 
 (defn comment-search [comments calls-left html-payload article-id]
-  (println calls-left)
   ; If calls-left == 0, return comments
   (if (<= calls-left 0)
     comments
@@ -199,27 +215,28 @@
           next-resp (-> (html/html-snippet
                          (:body @(http/get next-url {:insecure? true
                                                      :timeout 5000}))))]
-      (println (last comments-html))
-      (println last-id)
-      (println next-url)
       ; Tail Recursion - Performant
       (comment-search (concat comments new-comments)
                       (dec calls-left)
                       next-resp
                       article-id))))
 
-(defn get-comments-from-article [article-id]
-  (let [comment-url (format "https://www.sootoday.com/comments/load?Type=Comment&ContentId=%s&TagId=2346&TagType=Content&Sort=Oldest" article-id)
+(defn get-comments-from-article [{:keys [id url title]}]
+  (let [comment-url (format "https://www.sootoday.com/comments/load?Type=Comment&ContentId=%s&TagId=2346&TagType=Content&Sort=Oldest" id)
         initial-comment-payload (html/html-snippet
                                  (:body @(http/get comment-url {:insecure? true
                                                                 :timeout 5000})))
         ; This is the number of times we have to hit the API to get all top-level comments
-        num-comment-api-calls (num-comment-api-calls initial-comment-payload)]
-    (comment-search [] num-comment-api-calls initial-comment-payload article-id)))
+        num-comment-api-calls (num-comment-api-calls initial-comment-payload)
+        comments (comment-search []
+                                 num-comment-api-calls
+                                 initial-comment-payload
+                                 id)]
+    (log/info "Scraped [" title "] from - " url)
+    {:url url
+     :title title
+     :comments comments}))
 
-; https://www.sootoday.com/comments/load?Type=Comment&ContentId=1538042&TagId=2346&TagType=Content&Sort=Oldest
-; https://www.sootoday.com/comments/get?ContentId=1538042&TagId=2346&TagType=Content&Sort=Oldest&lastId=2586892
-; https://www.sootoday.com/comments/get?ContentId=1538042&TagId=2346&TagType=Content&Sort=Oldest&lastId=2591187
 (defn scrape-sootoday
   []
   (flatten
