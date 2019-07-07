4cc1dd80097fe3b88989ce555dbf18f823325174
Tyler Wilding <xtvaser@gmail.com>
2019-07-06 21:14:08 -0400

    linty lint lint

--- src/clj/salttoday/routes/api/v1/controller.clj
+++ src/clj/salttoday/routes/api/v1/controller.clj
@@ -2,7 +2,8 @@
   (:require [salttoday.db.comments :refer [get-comments]]
             [salttoday.db.users :refer [get-users]]
             [salttoday.routes.util :as routing-util]
-            [ring.util.http-response :as response]))
+            [ring.util.response :as response]
+            [ring.util.http-response :as http-response]))
 
 ; TODO Once we avoid the whole - passing in 0 instead of nil / the lack of a key
 ; we should make the parsing functions not require strings
@@ -19,7 +20,7 @@
         id-num (routing-util/string->number id)
         deleted-bool (routing-util/string->bool deleted)
         results (get-comments offset-num amount-num (keyword sort-type) days-num search-text user id-num deleted-bool)]
-    (-> (response/ok results)
+    (-> (http-response/ok results)
         (response/header "Content-Type"
                          "application/json"))))
 
@@ -31,6 +32,6 @@
   (let [offset-num (routing-util/string->number offset)
         amount-num (routing-util/string->number amount)
         days-num (routing-util/string->number days)]
-    (-> (response/ok (get-users offset-num amount-num (keyword sort-type) days-num))
+    (-> (http-response/ok (get-users offset-num amount-num (keyword sort-type) days-num))
         (response/header "Content-Type"
                          "application/json"))))
